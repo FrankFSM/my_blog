4346687c42859da747dbec013bc65dfeac7ad01d
RALAP\ralap <hao_jixiang@163.com>
2018-05-16 15:53:36 +0800

    Initial commit

---
+++ blog-admin/src/main/java/com/ralap/blog/core/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.ralap.blog.core.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * WebMvcConfig
+ * @author: ralap 
+ * @date: created at 2018/5/16 15:41
+ */
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+       registry.addViewController("/").setViewName("index");
+    }
+}
