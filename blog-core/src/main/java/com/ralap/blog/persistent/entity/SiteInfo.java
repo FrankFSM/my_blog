9778bc4d68a24a9508650953c8acc75784be7b5d
RALAP\ralap <hao_jixiang@163.com>
2018-06-01 09:57:37 +0800

    siteinfo

---
+++ blog-core/src/main/java/com/ralap/blog/persistent/entity/SiteInfo.java
@@ -0,0 +1,24 @@
+package com.ralap.blog.persistent.entity;
+
+import java.util.Date;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 网站信息
+ * @author: ralap
+ * @date: created at 2018/6/1 9:32
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class SiteInfo {
+
+    private Long articleCount;
+    private Integer tagCount;
+    private Integer typeCount;
+    private Long commentCount;
+    private Long onlineCount;
+    private Integer buildsiteDate;
+    private Date recodeTime;
+
+}
