0be39fee3bcba517f3f2ba4d038ff83b35821da8
RALAP\ralap <hao_jixiang@163.com>
2018-06-05 10:36:22 +0800

    security review

---
+++ blog-admin/src/main/java/com/ralap/blog/controller/SysUserController.java
@@ -0,0 +1,14 @@
+package com.ralap.blog.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: ralap
+ * @date: created at 2018/6/4 15:26
+ */
+@RestController
+public class SysUserController {
+
+
+
+}
